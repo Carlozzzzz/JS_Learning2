2bea089b7f9dc0b27c3dee15b61a2084b39d7d7e
carlozzzzz <carlosromulomaralit@gmail.com>
2024-06-07 09:33:56 +0800

    Initial Slot Machine Files

---
+++ Apps/SlotMachine/assets/js/main.js
@@ -0,0 +1,13 @@
+// Process/Steps:
+
+// 	1. Deposit some money
+// 	3. Determine the number of lines to bet
+// 	2. Collect the bet
+// 	4. Spin the slot machine
+// 	5. Check if the user won
+// 	6. Give the user's winnings
+// 	7. Play again
+
+// Roll each box for .3 sec in order by row
+// the roll for each row will be at the same time
+// 
